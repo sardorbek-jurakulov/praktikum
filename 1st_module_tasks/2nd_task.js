46c98899e4cd5e3f1d0e5f3940b158533efea6c6
“sardorbek-jurakulov” <sardorbek.juraqulov@gmail.com>
2024-06-22 11:03:07 +0500

    WIP

---
+++ 1st_module_tasks/2nd_task.js
@@ -0,0 +1,27 @@
+let dayOfTheWeek = 1;
+
+switch(dayOfTheWeek) {
+  case 1:
+    console.log('Dushanba');
+    break;
+  case 2:
+    console.log('Seshanba');
+    break;
+  case 3:
+    console.log('Chorshanba');
+    break;
+  case 4:
+    console.log('Payshanba');
+    break;
+  case 5:
+    console.log('Juma');
+    break;
+  case 6:
+    console.log('Shanba');
+    break;
+  case 7:
+    console.log('Yakshanba');
+    break;
+  default:
+    console.log('none');
+}
