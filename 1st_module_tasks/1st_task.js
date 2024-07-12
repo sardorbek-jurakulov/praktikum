bbeab68b816ae9d7ef0b0ff4c7bff07ed0437eb8
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2024-07-12 16:02:10 +0500

    WIP

--- 1st_module_tasks/1st_task.js
+++ 1st_module_tasks/1st_task.js
@@ -2,8 +2,9 @@ let a = 1;
 let b = 2;
 let c = 3;
 
-let result = a*100 + b*10 + c;
-
 if (a <= 0 || b <= 0 || c <=0 ) {
-  console.log(0);
+  return console.log(0);
 }
+
+let result = a*100 + b*10 + c;
+console.log(result);
--- 1st_module_tasks/4th_task.js
+++ 1st_module_tasks/4th_task.js
@@ -1,12 +1,18 @@
-let a = 1;
+let a = 999;
+
+if (!(a >= 1 && a <= 999)) {
+  return console.log('Kiritilgan son ruxsat etilgan chegarada(1-999) emas!');
+}
 
 
 function numberDigitsDeterminer(givenNumber) {
-  if ((givenNumber % 100) === 0) {
+  if ((Math.floor(givenNumber / 100) >= 1)) {
     return 3;
-  } else if ((givenNumber % 10) === 0) {
+  } else if ((Math.floor(givenNumber / 10) >= 1)) {
     return 2;
   } else {
     return 1;
   }
 }
+
+console.log(numberDigitsDeterminer(a));
