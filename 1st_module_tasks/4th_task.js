87d6b835f99b3815824be43078a8ae05d5277f84
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2024-07-12 16:09:14 +0500

    WIP

--- 1st_module_tasks/4th_task.js
+++ 1st_module_tasks/4th_task.js
@@ -1,33 +1,31 @@
-<<<<<<< HEAD
 let a = 999;
 
+// Yechimning 1 - varyanti
 if (!(a >= 1 && a <= 999)) {
   return console.log('Kiritilgan son ruxsat etilgan chegarada(1-999) emas!');
 }
-
 
 function numberDigitsDeterminer(givenNumber) {
   if ((Math.floor(givenNumber / 100) >= 1)) {
     return 3;
   } else if ((Math.floor(givenNumber / 10) >= 1)) {
-=======
-function numberDigitsDeterminer(givenNumber) {
-  if (givenNumber >= 100 && givenNumber < 1000) {
-    return 3;
-  } else if (givenNumber >= 10 && givenNumber < 100) {
->>>>>>> cc301f30607f088d10c65f7a7cfb9682e688344b
     return 2;
-  } else if (givenNumber >= 0 && givenNumber < 10) {
-    return 1;
   } else {
-    return 'Berilgan son shartga mos kelmaydi.'
+    return 1;
   }
 }
 
-<<<<<<< HEAD
-console.log(numberDigitsDeterminer(a));
-=======
-let a = 111;
+// Yechimning 2-varyanti
+// function numberDigitsDeterminer(givenNumber) {
+//   if (givenNumber >= 100 && givenNumber < 1000) {
+//     return 3;
+//   } else if (givenNumber >= 10 && givenNumber < 100) {
+//     return 2;
+//   } else if (givenNumber >= 1 && givenNumber < 10) {
+//     return 1;
+//   } else {
+//     return 'Berilgan son shartga mos kelmaydi.'
+//   }
+// }
 
 console.log(numberDigitsDeterminer(a));
->>>>>>> cc301f30607f088d10c65f7a7cfb9682e688344b
