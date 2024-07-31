b7bff787b8c4f41ae036a1f54e68ed0066ee3ca9
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2024-07-31 18:04:36 +0500

    WIP

--- 1st_module_tasks/14th_task.js
+++ 1st_module_tasks/14th_task.js
@@ -1,15 +1,15 @@
 let givenArray = [1, 0, -3, 21, -5];
-let sortedArray = [];
-
-for (let i = 0; i < givenArray.length; i++) {
-  let minValue = givenArray[i];
-  for (let j = i+1; j < givenArray.length; j++) {
-    if (givenArray[j] < minValue) {
-      minValue = givenArray[j];
-    }
-  }
-  sortedArray[i] = minValue;
-}
 
 console.log('Tartiblanmagan massiv: ', givenArray);
-console.log('Tartiblangan massiv: ', sortedArray);
+
+for (let i = 0; i < givenArray.length; i++) {
+  for (let j = i+1; j < givenArray.length; j++) {
+    if (givenArray[j] < givenArray[i]) {
+      givenArray[i] = givenArray[i] - givenArray[j];
+      givenArray[j] = givenArray[j] + givenArray[i];
+      givenArray[i] = givenArray[j] - givenArray[i];
+    }
+  }
+}
+
+console.log('Tartiblangan massiv: ', givenArray);
