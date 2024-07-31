49d2f2524e19600d19e7aa0235ce0ec92ac60d1a
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2024-07-31 18:09:16 +0500

    WIP

--- 1st_module_tasks/15th_task.js
+++ 1st_module_tasks/15th_task.js
@@ -1,1 +1,13 @@
 let givenArray = [1, 0, -3, 21, -5];
+
+for (let i = 0; i < givenArray.length; i++) {
+  for (let j = i+1; j < givenArray.length; j++) {
+    if (givenArray[j] < givenArray[i]) {
+      givenArray[i] = givenArray[i] - givenArray[j];
+      givenArray[j] = givenArray[j] + givenArray[i];
+      givenArray[i] = givenArray[j] - givenArray[i];
+    }
+  }
+}
+
+console.log(`Berilgan massiv [${givenArray}] qiymatlari ichida ikkinchi eng katta son bu -  `, givenArray[givenArray.length-2]);
